7bbd6b152ad427cf2ee379d2b727402f41e15873
Tóth Bence <tbencet96@gmail.com>
2019-02-06 18:23:21 +0100

    lab 1 hf megoldas

---
+++ Gyakorlo_Feladatok/Labor_1_OOP/OOP_Gyakorlo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Gyakorlo
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            Company c = new Company("E-Corp", 10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Worker w = new Worker("Worker_" + i, rnd.Next(100, 200));
+                w.Hours = rnd.Next(20, 30);
+                c.HireWorker(w);
+            }
+
+            Console.WriteLine(c.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Gyakorlo_Feladatok/Labor_1_OOP/OOP_Gyakorlo/Worker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Gyakorlo
+{
+    class Worker
+    {
+        public Worker(string name, int salary)
+        {
+            Name = name;
+            Wage = salary;
+        }
+
+        public string Name { get; set; }
+        public int Wage { get; private set; }
+        public int Hours { get; set; }
+        public string Address { get; set; }
+
+        public int Salary()
+        {
+            return Hours * Wage;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Name:{0}, Wage:{1}, Hours{2}", Name, Wage, Hours);
+        }
+    }
+}
