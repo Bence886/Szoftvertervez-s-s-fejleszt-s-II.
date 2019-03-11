2a493dafec89e45a52ddcf211dde0aa85c457388
Tóth Bence <tbencet96@gmail.com>
2019-03-11 11:25:23 +0100

    Lab_04

---
+++ Orai_Feladatok/Labor_04/DelegateEvent/DelegateEvent/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace DelegateEsemeny
+{
+    public delegate double Kozvetito(double szam);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kozvetito teszt = new Kozvetito(Muveletek.Ketszerezes);
+            //Kozvetito teszt = Muveletek.Ketszerezes;
+
+            Console.WriteLine(teszt(5));
+
+            SzovegFeldolgozo feldolgozo = Muveletek.NagyKiir;
+            feldolgozo += Muveletek.KisKiir;
+            feldolgozo("Teszt Szöveg!");
+
+            Szamolo szamolo = new Szamolo();
+            szamolo.figyelo += Szamolo_figyelo;
+            szamolo.Szam = 10;
+        }
+
+        private static void Szamolo_figyelo(int szamErtek)
+        {
+            Console.WriteLine(szamErtek);
+        }
+    }
+    static class Muveletek
+    {
+        public static double Ketszerezes(double szam)
+        {
+            return szam * szam;
+        }
+        public static void NagyKiir(string s)
+        {
+            Console.WriteLine(s.ToUpper());
+        }
+        public static void KisKiir(string s)
+        {
+            Console.WriteLine(s.ToLower());
+        }
+    }
+    delegate void SzovegFeldolgozo(string szoveg);
+
+    public delegate void SzamValtozott(int szamErtek);
+    class Szamolo
+    {
+        public event SzamValtozott figyelo;
+        int szam;
+        public int Szam
+        {
+            set {
+                szam = value;
+                if (figyelo != null)
+                {
+                    figyelo(szam);
+                }
+            }
+        }
+    }
+}
---
+++ Orai_Feladatok/Labor_04/InterfaceEvent/InterfaceEvent/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace InterfaceEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Phone A = new Phone(1000, "0123456789");
+            Phone B = new Phone(1000, "9876543210");
+
+            CallLog log = new CallLog();
+            A.RegisterListener(log);
+            B.RegisterListener(log);
+
+            A.StartCall(B);
+        }
+    }
+    interface ICallListener
+    {
+        void IncomingCall(Phone kuldo, string forras_telefonszam);
+        void OutgoindCall(Phone kuldo, string cel_telefonszam);
+    }
+    class Phone
+    {
+        string number;
+        int sum;
+        ICallListener callListener;
+        public Phone(int sum, string number)
+        {
+            Sum = sum;
+            Number = number;
+        }
+        public int Sum { get => sum; private set => sum = value; }
+        public string Number { get => number; private set => number = value; }
+
+        public void RegisterListener(ICallListener listener)
+        {
+            callListener = listener;
+        }
+        private void AcceptCall(Phone source)
+        {
+            if (callListener != null)
+            {
+                callListener.IncomingCall(source, source.Number);
+            }
+        }
+        public void StartCall(Phone cel)
+        {
+            callListener.OutgoindCall(cel, cel.Number);
+            if (sum > 100)
+            {
+                sum -= 100;
+                cel.AcceptCall(this);
+            }
+        }
+        public override string ToString()
+        {
+            return string.Format("Telefonszam:{0}, egyenleg:{1}", number, sum);
+        }
+    }
+
+    class CallLog : ICallListener
+    {
+        public void IncomingCall(Phone sender, string source_number)
+        {
+            Console.WriteLine("Hívás innen: " + sender.ToString());
+        }
+
+        public void OutgoindCall(Phone sender, string destination_number)
+        {
+            Console.WriteLine("Hívom ezt: " + sender.ToString());
+        }
+    }
+}
